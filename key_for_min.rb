9a03897a602d1f0441a1a683558c034e523a2865
Cody Fussell <cody.fussell@gmail.com>
2019-09-16 23:40:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,12 @@ def key_for_min_value(h)
     lowest_k = []
     h.each{|k,v|
         if lowest_v[0] == 0
-            lowest_v[0] = v, lowest_k[0] = k
+            lowest_v[0] = v
+            lowest_k[0] = k
         elsif v < lowest_v[0]
-            lowest_v[0] = v, lowest_k[0] = k
+            lowest_v[0] = v
+            lowest_k[0] = k
         end
     }    
     return lowest_k[0]    
-end 
+end
